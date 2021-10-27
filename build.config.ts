c199e55a04fa032ad0a94e32ef0d8892705adaf8
Pooya Parsa <pyapar@gmail.com>
2021-10-27 23:24:53 +0200

    refactor: use unbuild

---
+++ build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  emitCJS: true,
+  inlineDependencies: true,
+  entries: [
+    'src/index'
+  ]
+})
