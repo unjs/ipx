a7da5e9940c87edef97f317a1ee00e683d8d5cfc
Pooya Parsa <pyapar@gmail.com>
2022-06-20 17:22:27 +0200

    test: use local server

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,23 +1,25 @@
+import { listen } from 'listhen'
 import { resolve } from 'pathe'
+import { describe, it, expect, beforeAll } from 'vitest'
+import serveHandler from 'serve-handler'
 import { IPX, createIPX } from '../src'
 
 describe('ipx', () => {
   let ipx: IPX
-  it('createIPX', () => {
+  beforeAll(() => {
     ipx = createIPX({
       dir: resolve(__dirname, 'assets'),
-      domains: ['raw.githubusercontent.com'],
-      alias: {
-        'gh-raw': 'https://raw.githubusercontent.com'
-      }
+      domains: ['localhost']
     })
   })
 
   it('remote file', async () => {
-    const src = await ipx('gh-raw/unjs/ipx/main/test/assets/bliss.jpg')
+    const listener = await listen((req, res) => { serveHandler(req, res, { public: resolve(__dirname, 'assets') }) }, { port: 0 })
+    const src = await ipx(`${listener.url}/bliss.jpg`)
     const { data, format } = await src.data()
     expect(data).toBeInstanceOf(Buffer)
     expect(format).toBe('jpeg')
+    await listener.close()
   })
 
   it('local file', async () => {
