c06363c4b7847f2153b7d0d286e1340a756bfccc
Thorn Walli <lammpee@gmail.com>
2022-06-20 16:24:12 +0200

    chore(test): add remote file (#60)

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,11 +5,22 @@ describe('ipx', () => {
   let ipx: IPX
   it('createIPX', () => {
     ipx = createIPX({
-      dir: resolve(__dirname, 'assets')
+      dir: resolve(__dirname, 'assets'),
+      domains: ['raw.githubusercontent.com'],
+      alias: {
+        'gh-raw': 'https://raw.githubusercontent.com'
+      }
     })
   })
 
-  it('data', async () => {
+  it('remote file', async () => {
+    const src = await ipx('gh-raw/unjs/ipx/main/test/assets/bliss.jpg')
+    const { data, format } = await src.data()
+    expect(data).toBeInstanceOf(Buffer)
+    expect(format).toBe('jpeg')
+  })
+
+  it('local file', async () => {
     const src = await ipx('bliss.jpg')
     const { data, format } = await src.data()
     expect(data).toBeInstanceOf(Buffer)
