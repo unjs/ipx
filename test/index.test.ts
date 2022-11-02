30d657d6d827b95d34d663bc2f6f47b882bb558f
pooya parsa <pyapar@gmail.com>
2022-11-02 11:04:51 +0100

    Update test/index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -14,7 +14,7 @@ describe('ipx', () => {
   })
 
   it('remote file', async () => {
-    const listener = await listen((req, res) => { serveHandler(req, res, { public: resolve(__dirname, 'assets') }) }, { port: 3000, hostname: 'localhost' })
+    const listener = await listen((req, res) => { serveHandler(req, res, { public: resolve(__dirname, 'assets') }) }, { port: 0 })
     const src = await ipx(`${listener.url}/bliss.jpg`)
     const { data, format } = await src.data()
     expect(data).toBeInstanceOf(Buffer)
