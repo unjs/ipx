fb3cf1d0e1d2bc9cedf94f0a2e2896d6388ea675
Pooya Parsa <pooya@pi0.io>
2023-10-18 00:43:44 +0200

    fix(server): append `vary` header instead of overriding it

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import {
   H3Event,
   H3Error,
   send,
+  appendResponseHeader,
 } from "h3";
 import { IPX } from "./ipx";
 
@@ -70,7 +71,7 @@ export function createIPXH3Handler(ipx: IPX) {
       delete modifiers.format;
       if (autoFormat) {
         modifiers.format = autoFormat;
-        setResponseHeader(event, "vary", "Accept");
+        appendResponseHeader(event, "vary", "Accept");
       }
     }
 
