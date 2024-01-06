2c6b547825e5c46f6cd1e416c8a81aedd752ae41
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2024-01-06 12:59:07 +0800

    Improve unstorage driver

--- src/storage/unstorage.ts
+++ src/storage/unstorage.ts
@@ -1,29 +1,72 @@
 import type { Storage, Driver } from "unstorage";
+import { createError } from "h3";
 import type { IPXStorage, IPXStorageMeta } from "../types";
+
+export type UnstorageIPXStorageOptions = {
+  prefix: string | undefined;
+};
+
+async function parseData(data: any) {
+  // Known possible data types:
+  // ArrayBuffer, Buffer, String, Blob, any
+  // Ref: https://github.com/unjs/unstorage/tree/main/src/drivers
+
+  if (!data) {
+    // File not found, do not attempt to parse
+    return;
+  }
+
+  if (data instanceof Blob) {
+    data = await data.arrayBuffer();
+  }
+
+  try {
+    // IPX requires a Buffer, attempt to parse in storage layer to normalize expected error
+    return Buffer.from(data);
+  } catch (error: any) {
+    throw createError({
+      statusCode: 500,
+      statusText: `IPX_STORAGE_ERROR`,
+      message: `Failed to parse storage data to Buffer:\n${error.message}`,
+    });
+  }
+}
 
 export function unstorageToIPXStorage(
   storage: Storage | Driver,
-  prefix: string,
+  options?: UnstorageIPXStorageOptions,
 ): IPXStorage {
-  const resolveKey = (id: string) => prefix + ":" + id;
+  const resolveKey = (id: string) => {
+    id = id.replaceAll("/", ":");
+
+    if (options && options.prefix) {
+      return `${options.prefix}:${id}`;
+    }
+
+    return id;
+  };
+
   return {
     name: "ipx:" + ((storage as any).name || "unstorage"),
     async getMeta(id, opts = {}) {
       if (!storage.getMeta) {
         return;
       }
+
       const storageKey = resolveKey(id);
       const meta = await storage.getMeta(storageKey, opts);
+
       return meta as IPXStorageMeta;
     },
     async getData(id, opts = {}) {
       if (!storage.getItemRaw) {
         return;
       }
+
       const storageKey = resolveKey(id);
       const data = await storage.getItemRaw(storageKey, opts);
-      // TODO: Convert Buffer to ArrayBuffer
-      return data as ArrayBuffer;
+
+      return parseData(data);
     },
   };
 }
---
+++ test/unstorage.test.ts
@@ -0,0 +1,90 @@
+import { resolve } from "pathe";
+import { describe, it, expect, beforeAll } from "vitest";
+import { createStorage } from "unstorage";
+import fsLiteDriver from "unstorage/drivers/fs-lite";
+import githubDriver from "unstorage/drivers/github";
+import httpDriver from "unstorage/drivers/http";
+import { IPX, createIPX, unstorageToIPXStorage } from "../src";
+
+function getFile() {
+  // eslint-disable-next-line unicorn/prefer-module
+  const driver = fsLiteDriver({ base: resolve(__dirname, "assets") });
+  const storage = createStorage({ driver });
+  return storage.getItemRaw("bliss.jpg");
+}
+
+type TestVariant = {
+  name: string;
+  setup: () => IPX | Promise<IPX>;
+  condition?: () => boolean;
+};
+
+const variants = [
+  {
+    name: "node fs lite",
+    setup: () => {
+      // eslint-disable-next-line unicorn/prefer-module
+      const driver = fsLiteDriver({ base: resolve(__dirname, "assets") });
+      const storage = createStorage({ driver });
+      return createIPX({ storage: unstorageToIPXStorage(storage) });
+    },
+  },
+  {
+    name: "memory",
+    setup: async () => {
+      const storage = createStorage();
+      await storage.setItemRaw("bliss.jpg", await getFile());
+      return createIPX({ storage: unstorageToIPXStorage(storage) });
+    },
+  },
+  {
+    name: "github",
+    setup: () => {
+      const driver = githubDriver({ repo: "unjs/ipx", dir: "test/assets" });
+      const storage = createStorage({ driver });
+      return createIPX({ storage: unstorageToIPXStorage(storage) });
+    },
+    condition: () => process.env.TEST_UNSTORAGE_GITHUB === "true",
+  },
+  {
+    name: "http",
+    setup: () => {
+      const driver = httpDriver({
+        base: "https://raw.githubusercontent.com/unjs/ipx/main/test/assets",
+      });
+      const storage = createStorage({ driver });
+      return createIPX({ storage: unstorageToIPXStorage(storage) });
+    },
+    condition: () => process.env.TEST_UNSTORAGE_HTTP === "true",
+  },
+] satisfies TestVariant[];
+
+describe.each(variants)(`unstorage: $name`, ({ setup, condition }) => {
+  const shouldRun = condition ? condition() : true;
+  let ipx: IPX;
+
+  beforeAll(async () => {
+    ipx = await setup();
+  });
+
+  it.runIf(shouldRun)("file found", async () => {
+    const source = await ipx("bliss.jpg");
+    const { data, format } = await source.process();
+    expect(data).toBeInstanceOf(Buffer);
+    expect(format).toBe("jpeg");
+  });
+
+  it.runIf(shouldRun)("file not found", async () => {
+    const source = await ipx("unknown.jpg");
+    await expect(() => source.process()).rejects.toThrowError(
+      "Resource not found: /unknown.jpg",
+    );
+  });
+
+  it.runIf(shouldRun)("invalid path", async () => {
+    const source = await ipx("*.jpg");
+    await expect(() => source.process()).rejects.toThrowError(
+      "Resource not found: /*.jpg",
+    );
+  });
+});
