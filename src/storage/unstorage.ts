be4b092c31a809db7b13f85bd2eb13f87c50b032
Arkadiusz Sygulski <aareksio@gmail.com>
2024-01-12 06:49:48 +0700

    fix: improve data parsing for unstorage (#204)

--- src/storage/unstorage.ts
+++ src/storage/unstorage.ts
@@ -1,11 +1,21 @@
 import type { Storage, Driver } from "unstorage";
+import { createError } from "h3";
 import type { IPXStorage, IPXStorageMeta } from "../types";
+
+export type UnstorageIPXStorageOptions = {
+  prefix?: string;
+};
 
 export function unstorageToIPXStorage(
   storage: Storage | Driver,
-  prefix: string,
+  _options: UnstorageIPXStorageOptions | string = {},
 ): IPXStorage {
-  const resolveKey = (id: string) => prefix + ":" + id;
+  const options =
+    typeof _options === "string" ? { prefix: _options } : _options;
+
+  const resolveKey = (id: string) =>
+    options.prefix ? `${options.prefix}:${id}` : id;
+
   return {
     name: "ipx:" + ((storage as any).name || "unstorage"),
     async getMeta(id, opts = {}) {
@@ -21,9 +31,30 @@ export function unstorageToIPXStorage(
         return;
       }
       const storageKey = resolveKey(id);
-      const data = await storage.getItemRaw(storageKey, opts);
-      // TODO: Convert Buffer to ArrayBuffer
-      return data as ArrayBuffer;
+
+      // Known possible data types: ArrayBuffer, Buffer, String, Blob
+      let data = await storage.getItemRaw(storageKey, opts);
+
+      if (!data) {
+        // File not found, do not attempt to parse
+        return;
+      }
+
+      if (data instanceof Blob) {
+        data = await data.arrayBuffer();
+      }
+
+      try {
+        // IPX requires a Buffer, attempt parse and normalize error
+        return Buffer.from(data as ArrayBuffer);
+      } catch (error: any) {
+        throw createError({
+          statusCode: 500,
+          statusText: `IPX_STORAGE_ERROR`,
+          message: `Failed to parse storage data to Buffer:\n${error.message}`,
+          cause: error,
+        });
+      }
     },
   };
 }
