912203e03f42a8bc1c173777053065c966949ecf
Arkadiusz Sygulski <aareksio@gmail.com>
2024-01-12 07:15:12 +0700

    feat(node-fs): add support for multiple dirs (#203)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/storage/node-fs.ts
+++ src/storage/node-fs.ts
@@ -1,72 +1,79 @@
-import type { Stats } from "node:fs";
 import { resolve, parse, join } from "pathe";
 import { createError } from "h3";
 import { cachedPromise, getEnv } from "../utils";
 import type { IPXStorage } from "../types";
 
 export type NodeFSSOptions = {
-  dir?: string;
+  dir?: string | string[];
   maxAge?: number;
 };
 
 export function ipxFSStorage(_options: NodeFSSOptions = {}): IPXStorage {
-  const rootDir = resolve(_options.dir || getEnv("IPX_FS_DIR") || ".");
+  const dirs = resolveDirs(_options.dir);
   const maxAge = _options.maxAge || getEnv("IPX_FS_MAX_AGE");
 
-  const _resolve = (id: string) => {
-    const resolved = join(rootDir, id);
-    if (!isValidPath(resolved) || !resolved.startsWith(rootDir)) {
+  const _getFS = cachedPromise(() =>
+    import("node:fs/promises").catch(() => {
       throw createError({
-        statusCode: 403,
-        statusText: `IPX_FORBIDDEN_PATH`,
-        message: `Forbidden path: ${id}`,
+        statusCode: 500,
+        statusText: `IPX_FILESYSTEM_ERROR`,
+        message: `Failed to resolve filesystem module`,
       });
-    }
-    return resolved;
-  };
+    }),
+  );
 
-  const _getFS = cachedPromise(() => import("node:fs/promises"));
+  const resolveFile = async (id: string) => {
+    const fs = await _getFS();
+    for (const dir of dirs) {
+      const filePath = join(dir, id);
+      if (!isValidPath(filePath) || !filePath.startsWith(dir)) {
+        throw createError({
+          statusCode: 403,
+          statusText: `IPX_FORBIDDEN_PATH`,
+          message: `Forbidden path: ${id}`,
+        });
+      }
+      try {
+        const stats = await fs.stat(filePath);
+        if (!stats.isFile()) {
+          // Keep looking in other dirs we are looking for a file!
+          continue;
+        }
+        return {
+          stats,
+          read: () => fs.readFile(filePath),
+        };
+      } catch (error: any) {
+        if (error.code === "ENOENT") {
+          // Keep looking in other dirs
+          continue;
+        }
+        throw createError({
+          statusCode: 403,
+          statusText: `IPX_FORBIDDEN_FILE`,
+          message: `Cannot access file: ${id}`,
+        });
+      }
+    }
+    throw createError({
+      statusCode: 404,
+      statusText: `IPX_FILE_NOT_FOUND`,
+      message: `File not found: ${id}`,
+    });
+  };
 
   return {
     name: "ipx:node-fs",
     async getMeta(id) {
-      const fsPath = _resolve(id);
-
-      let stats: Stats;
-      try {
-        const fs = await _getFS();
-        stats = await fs.stat(fsPath);
-      } catch (error: any) {
-        throw error.code === "ENOENT"
-          ? createError({
-              statusCode: 404,
-              statusText: `IPX_FILE_NOT_FOUND`,
-              message: `File not found: ${id}`,
-            })
-          : createError({
-              statusCode: 403,
-              statusText: `IPX_FORBIDDEN_FILE`,
-              message: `File access forbidden: (${error.code}) ${id}`,
-            });
-      }
-      if (!stats.isFile()) {
-        throw createError({
-          statusCode: 400,
-          statusText: `IPX_INVALID_FILE`,
-          message: `Path should be a file: ${id}`,
-        });
-      }
-
+      const { stats } = await resolveFile(id);
       return {
         mtime: stats.mtime,
         maxAge,
       };
     },
     async getData(id) {
-      const fsPath = _resolve(id);
-      const fs = await _getFS();
-      const contents = await fs.readFile(fsPath);
-      return contents;
+      const { read } = await resolveFile(id);
+      return read();
     },
   };
 }
@@ -84,4 +91,12 @@ function isValidPath(fp: string) {
     return false;
   }
   return true;
+}
+
+function resolveDirs(dirs?: string | string[]) {
+  if (!dirs || !Array.isArray(dirs)) {
+    const dir = resolve(dirs || getEnv("IPX_FS_DIR") || ".");
+    return [dir];
+  }
+  return dirs.map((dirs) => resolve(dirs));
 }
